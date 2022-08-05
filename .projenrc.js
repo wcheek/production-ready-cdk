1eaf1c13fd0628abd7a7f601d01a5568fb8e05f3
Wesley Cheek <66931618+wcheek@users.noreply.github.com>
2022-08-05 11:50:02 +0900

    Update packages

--- .projenrc.js
+++ .projenrc.js
@@ -1,8 +1,17 @@
 const { awscdk } = require("projen");
 const project = new awscdk.AwsCdkTypeScriptApp({
+  authorName: 'Wesley Cheek',
   cdkVersion: "2.35.0",
   defaultReleaseBranch: "main",
   name: "production-ready-cdk",
+  description: 'A CDK project to learn production CDK',
+  repositoryUrl: 'https://github.com/wcheek/production-ready-cdk.git',
+  keywords: [
+    'AWS CDK',
+    'projen',
+    'Typescript',
+    'Deployment',
+  ],
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
